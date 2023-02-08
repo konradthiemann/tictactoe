c37c3b1d6c8c2f4b885a6a91e3e6ac7381c35d55
Konrad Thiemann <konrad.gruss@t-online.de>
2023-02-08 11:28:51 +0100

    fill boxes available

---
+++ scipt.js
@@ -0,0 +1,24 @@
+let fields =[];
+let currentShape = 'cross';
+
+function fillShape(id){
+    if(currentShape == 'cross'){
+        currentShape = 'circle';
+    }else{
+        currentShape = 'cross';
+    }
+
+    fields[id] = currentShape;
+}
+
+function draw(){
+    for(let i = 0; i < fields.length; i++){
+        if(fields[i] == 'circle'){
+            document.getElementById('circle-' + i).classList.remove('hide');
+        }
+
+        if(fields[i] == 'cross'){
+            document.getElementById('cross-' + i).classList.remove('hide');
+        }
+    }
+}
